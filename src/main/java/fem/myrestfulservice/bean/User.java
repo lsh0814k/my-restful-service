e1f5d72ddba6e2eb4ef95b8b6493648db6ff2918
이상훈 <slee@i-magicsoft.com>
2024-01-19 10:31:31 +0900

    swagger 사용

--- src/main/java/fem/myrestfulservice/bean/User.java
+++ src/main/java/fem/myrestfulservice/bean/User.java
@@ -1,6 +1,7 @@
 package fem.myrestfulservice.bean;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
@@ -10,13 +11,22 @@ import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@Schema(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
+    @Schema(title = "사용자 ID", description = "사용자 ID는 자동 생성됩니다.")
     private Long id;
+
+    @Schema(title = "사용자 이름", description = "사용자 이름을 입력합니다.")
     @Min(value = 2, message = "name은 2글자 이상 입력해주세요.")
     private String name;
+
+    @Schema(title = "사용자 등록일", description = "사용자 등록일을 입력할 수 있습니다. 입력하지 않으면 현재 날짜가 지정됩니다.")
     @Past(message = "등록일은 미래 날짜를 입력할 수 없습니다.")
     private LocalDateTime joinDate;
 
+    @Schema(title = "사용자 비밀번호", description = "사용자의 비밀번호를 입력합니다.")
     private String password;
+
+    @Schema(title = "사용자 주민번호", description = "사용자의 주민번호를 입력합니다.")
     private String ssn;
 }
--- src/main/java/fem/myrestfulservice/controller/UserController.java
+++ src/main/java/fem/myrestfulservice/controller/UserController.java
@@ -3,10 +3,14 @@ package fem.myrestfulservice.controller;
 import fem.myrestfulservice.bean.User;
 import fem.myrestfulservice.dao.UserDaoService;
 import fem.myrestfulservice.exception.UserNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -14,11 +18,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
+@Tag(name = "user-controller", description = "일반 사용자 서비스를 위한 컨트롤러 입니다.")
 public class UserController {
     private final UserDaoService userDaoService;
 
@@ -27,8 +33,16 @@ public class UserController {
         return userDaoService.findAll();
     }
 
+    @Operation(summary = "사용자 정보 조회 API", description = "사용자 ID를 이용해서 사용자 상세 정보 조회를 합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+            @ApiResponse(responseCode = "404", description = "USER NOT FOUND"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+    })
     @GetMapping("/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable Long id) {
+    public EntityModel<User> retrieveUser(
+            @Parameter(description = "사용자 ID", required = true, example = "1") @PathVariable Long id) {
         User user = userDaoService.findOne(id);
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
