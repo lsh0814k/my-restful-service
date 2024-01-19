a4c2a41e5b3052ade0e0e55a1c4a9188d2fbe6b4
이상훈 <slee@i-magicsoft.com>
2024-01-19 12:49:14 +0900

    jpa를 이용한 초기데이터

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into users(id, join_date, name, password, ssn) values (1, current_timestamp(), 'user1', '123456', '931111-1111111');
+insert into users(id, join_date, name, password, ssn) values (2, current_timestamp(), 'user2', '123456', '941111-1111111');
+insert into users(id, join_date, name, password, ssn) values (3, current_timestamp(), 'user3', '123456', '951111-1111111');
+insert into users(id, join_date, name, password, ssn) values (4, current_timestamp(), 'user4', '123456', '961111-1111111');
