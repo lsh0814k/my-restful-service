03f8543b919153314cf49931d50b9fcbb4dab18f
이상훈 <slee@i-magicsoft.com>
2024-01-19 13:25:58 +0900

    jpa를 이용하여 조회 생성 삭제 기능 추가

--- src/main/java/fem/myrestfulservice/controller/UserJpaController.java
+++ src/main/java/fem/myrestfulservice/controller/UserJpaController.java
@@ -1,22 +1,62 @@
 package fem.myrestfulservice.controller;
 
 import fem.myrestfulservice.bean.User;
+import fem.myrestfulservice.exception.UserNotFoundException;
 import fem.myrestfulservice.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
-@RequestMapping("/jpa")
+@RequestMapping("/jpa/users")
 @RequiredArgsConstructor
 public class UserJpaController {
     private final UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> retrieveAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel> retrieveUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        EntityModel<User> entityModel = EntityModel.of(optionalUser.get());
+        return ResponseEntity.ok(entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users")));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isEmpty()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        userRepository.deleteById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody @Valid User user) {
+        User savedUser = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(location).build();
     }
 }
